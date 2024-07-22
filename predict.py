d487cbfc0a549178cfe67d6fd5e9bf8e4103fb00
Lwasampijja Baker <bubakes2000@gmail.com>
2024-07-22 12:06:14 +0300

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,15 +1,16 @@
-import os
-
-os.environ["SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL"] = "True"
-
 import streamlit as st
 import pandas as pd
 import joblib
 from io import StringIO
 from PIL import Image
+from joblib import load
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Load the saved model
-model = joblib.load('files/hash_gbc.pkl')
+#model = joblib.load('files/hash_final.pkl')
+model = load('files/hash_model.joblib')
 
 # Set up the sidebar
 # Logo image
@@ -22,9 +23,14 @@ st.sidebar.markdown("""
 """)
 
 
+# Display HASH logo image
+logo_url = 'files/hash.png'
+st.image(logo_url, width=200) 
+
+
 # Main content
 st.write("""
-### Machine Learning algorithms for predictive modeling of the ability to pay for Pre-Exposure (PrEP) services, a pilot study in Kampala, Uganda.
+### A predictive model for one's ability to pay for Pre-Exposure Prophylactic (PrEP) services in Uganda.
          """)
 
 # Project Description and Column Key
@@ -32,22 +38,87 @@ st.write("""
 #### Project Description
 HIV prevention services in Uganda including PrEP have remained largely donor-funded. With the changing trends in healthcare financing, there is a need for more sustainable health care financing models. Understanding the nuances in PrEP service delivery especially targeting unserved high risk sub-populations through innovative approaches like cost-sharing, privately purchased etc is worth a venture to explore.
 
-We set to use machine learning and AI models on both public and private datasets to build models that would predict the ability to pay for HIV prevention services particularly PrEP. 
+This AI Model was trained on both public and private datasets to predict ability to pay for HIV prevention services particularly PrEP. 
 """)
 
 
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     # Display the uploaded data
-    st.write('Uploaded Data:')
-    st.write(data)
+    st.write(' #### Uploaded Data Sample:')
+    st.write(data.head())
+
+    # Display the uploaded data shape
+    st.write(' #### Number of rows and columns:')
+    st.write(data.shape)
+
+    st.write(' #### Data Summary:')
+    st.write(data.describe())
+
+    # Display column names and types
+    st.write(' #### Column Names and Data Types:')
+    # Capture the output of data.info()
+    buffer = StringIO()
+    data.info(buf=buffer)
+    s = buffer.getvalue()
+    st.text(s)
+
+    # Plot the gender pie chart
+    st.write(""" #### Gender Disribution:
+             Key: 1 = Male, 2 = Female, 99 = Missing
+             """)
+    plt.figure(figsize=(15,10))
+    data['gender'].value_counts().plot(kind='pie', autopct='%.2f%%')
+    plt.title('Gender Distribution')
+    plt.ylabel('')  # Hide the y-label for a cleaner look
+
+    # Display the plot in Streamlit
+    st.pyplot(plt)
+
+     # Empoloyment Status
+    st.write(""" #### Employment Status:
+                    1 - PROFESSIONAL   
+                2 - CLERICAL
+                3 - SALES AND SERVICES
+                4 - SKILLED MANUAL
+                5 - UNSKILLED MANUAL
+                6 - DOMESTIC SERVICE
+                7 - AGRICULTURE
+                96 - OTHER (SPECIFY)
+             """)
+    plt.figure(figsize=(15,10))
+    data['occuptn'].value_counts().head(30).plot(kind='barh', figsize=(20,10))
+
+    # Calculate the percentage for each bar
+    total = data['occuptn'].value_counts().sum()
+    for i, v in enumerate(data['occuptn'].value_counts().head(30)):
+        percentage = f"{(v / total) * 100:.2f}%"
+        plt.text(v, i, percentage, color='black', va='center')
+
+    # Display the plot in Streamlit
+    st.pyplot(plt)
+    
+    # Plot the gender pie chart
+    st.write(""" #### Residence Disribution:
+             Key: 1 = Urban, 2 = Rural
+             """)
+    # Plot the count plot
+    plt.figure(figsize=(15, 10))
+    sns.countplot(data=data, x='urban')
+    plt.xlabel('Residence')
+    plt.ylabel('Count of Respondents')
+
+    # Display the plot in Streamlit
+    st.pyplot(plt)
 
     # Make predictions using the loaded model and add a 'Prediction' column
     predictions = model.predict(data)  # Assuming 'model' is a trained model object
     data['Prediction'] = predictions
 
     # Display the predictions
-    st.write('Predictions:')
+    st.write(""" #### Predictions:
+             Key: 1 = Able to Pay, 0 = Not able to Pay
+             """)
     st.write(data)
 
     # Export results to a CSV file
